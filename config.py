93f201ae5feb0d05a85ea91e706345310aeec655
Elton de Andrade Rodrigues <eltonrodrigues61@gmail.com>
2019-05-06 15:32:30 -0300

    update lib

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     Common configurations
     """
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@db/pycastdb'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/pycastdb'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
